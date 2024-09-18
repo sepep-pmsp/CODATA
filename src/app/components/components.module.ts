35b286e542f4914d90822c16d6aeaa950eb299b6
Ana Carolina <carol00_10@hotmail.com>
2024-09-18 15:57:21 -0300

    declarando os componentes

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { ProjectComponent } from './projetos/project/project.component';
+import { ItensProjectsComponent } from './projetos/itens-projects/itens-projects.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, ProjectComponent, ItensProjectsComponent, HomeComponent],
   imports: [
     CommonModule
   ],
