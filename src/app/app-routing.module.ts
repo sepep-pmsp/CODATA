61299a5cee60661eda700340b6632057ffe62bdf
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2024-09-19 09:02:41 -0300

    arranging the routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { PagesProjectsComponent } from './components/projetos/pages-projects/pages-projects.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full' , component: HomeComponent }
+  { path: '', pathMatch: 'full' , component: HomeComponent },
+  { path: 'projetos/:id', component: PagesProjectsComponent }
 ];
 
 @NgModule({
