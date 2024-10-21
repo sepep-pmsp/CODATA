76df6d8e706d85b6ab606923db3980f0bb3ba175
Ana Carolina <carol00_10@hotmail.com>
2024-10-21 17:52:40 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/body/home/home.component';
-import { PagesProjectsComponent } from './components/projetos/pages-projects/pages-projects.component';
+
 
 const routes: Routes = [
   { path: '', pathMatch: 'full' , component: HomeComponent },
-  { path: 'projetos/:id', component: PagesProjectsComponent }
 ];
 
 @NgModule({
