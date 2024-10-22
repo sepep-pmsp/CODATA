0b50fd2d71ea48dbde919d79f45241f7167c683a
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2024-10-22 16:18:48 -0300

    placing routes and pop-ups on cards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/body/home/home.component';
+import { ContentPageComponent } from './components/body/content-page/content-page.component';
 
 
 const routes: Routes = [
   { path: '', pathMatch: 'full' , component: HomeComponent },
+  { path: 'pop-up/:slug', component: ContentPageComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/components/body/content-page/content-page.component.ts
+++ src/app/components/body/content-page/content-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-content-page',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrl: './content-page.component.scss'
 })
 export class ContentPageComponent {
+  openedPopupSlug: string | null = null;
+
+  popupMapping: { [key: string]: number } = {
+    'dados': 0,
+    'limpeza': 1,
+    'outro-slug': 2,
+  };
+  openedPopupIndex: null | undefined;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const slug = params['slug'];
+      if (slug && this.popupMapping[slug] !== undefined) {
+        this.openPopup(slug);
+      }
+    });
+  }
+
+  openPopup(slug: string) {
+    this.openedPopupSlug = slug;
+    this.router.navigate(['/pop-up', slug]);
+  }
+
+  closePopup() {
+    this.openedPopupSlug = null;
+    this.router.navigate(['/']);
+  }
+  isPopupOpen(slug: string): boolean {
+    return this.openedPopupSlug === slug;
+  }
 
 }
