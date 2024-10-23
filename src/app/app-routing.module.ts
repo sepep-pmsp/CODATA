8d59bc506a1909637bae4abe98b7f5714125d3a2
Ana Carolina <121954394+Ana-CAS-Machado@users.noreply.github.com>
2024-10-23 19:56:24 -0300

    6 home page (#9)
    * arranging page routes
    
    * Update app-routing.module.ts
    
    * Create content-page.component.html
    
    * Create content-page.component.scss
    
    * Create content-page.component.ts
    
    * Update header.component.scss
    
    * Update home.component.html
    
    * Update components.module.ts
    
    * placing routes and pop-ups on cards
    
    * feat(html): refactors repeated block structure using *ngFor
    
    - Replaces repeated blocks of content in the activity area with *ngFor to dynamically generate cards.
    - Refactor the small block area to use *ngFor with dynamic data in the TypeScript file.
    - Reduces the size of the main HTML while keeping the classes and structure intact.
    
    * refactor(scss): optimizes SCSS by removing redundancies and grouping selectors
    
    - Groups common selectors like .repositories, .projects, and .partners to reduce duplication.
    - Removes redundant CSS properties and simplifies the nesting structure.
    - Preserves layout and overall appearance while minimizing repetitive code.
    
    * feat(ts): creates dynamic arrays for content generated in HTML with *ngFor
    
    - Defines dynamic arrays in TypeScript to generate cards and small blocks in HTML.
    - Moves text and number data into TypeScript, making it easier to maintain and expand.
    - Implements date pipes for automatic date formatting in HTML.
    
    * Update content-page.component.scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { PagesProjectsComponent } from './components/projetos/pages-projects/pages-projects.component';
+import { HomeComponent } from './components/body/home/home.component';
+import { ContentPageComponent } from './components/body/content-page/content-page.component';
+
 
 const routes: Routes = [
   { path: '', pathMatch: 'full' , component: HomeComponent },
-  { path: 'projetos/:id', component: PagesProjectsComponent }
+  { path: 'pop-up/:slug', component: ContentPageComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/components/project/bd-page/bd-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bd-page',
+  templateUrl: './bd-page.component.html',
+  styleUrl: './bd-page.component.scss'
+})
+export class BdPageComponent {
+
+}
