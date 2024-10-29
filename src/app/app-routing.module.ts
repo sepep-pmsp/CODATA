3281ab5661ea077c312d8ae6c1c0923cd2d838e6
Ana Carolina <carol00_10@hotmail.com>
2024-10-28 22:13:16 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/body/home/home.component';
 import { ContentPageComponent } from './components/body/content-page/content-page.component';
+import { ProjectsComponent } from './components/project/project';
 
 
 const routes: Routes = [
   { path: '', pathMatch: 'full' , component: HomeComponent },
   { path: 'pop-up/:slug', component: ContentPageComponent },
+  { path: 'projetos', component: ProjectsComponent },
 ];
 
 @NgModule({
