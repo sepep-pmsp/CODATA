46af4875d157d82cc1d340222e55795bd429649f
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2024-11-04 14:55:13 -0300

    Update sistemas-page.component.ts

--- src/app/components/project/sistema/sistemas-page/sistemas-page.component.ts
+++ src/app/components/project/sistema/sistemas-page/sistemas-page.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Sistemas } from '../sistemas.model';
+import { ActivatedRoute } from '@angular/router';
+import { SistemasService } from '../sistemas-service.service';
 
 @Component({
   selector: 'app-sistemas-page',
   templateUrl: './sistemas-page.component.html',
   styleUrl: './sistemas-page.component.scss'
 })
-export class SistemasPageComponent {
+export class SistemasPageComponent implements OnInit {
+  sistema: Sistemas | undefined;
 
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private sistemasService: SistemasService
+  ) {}
+
+  ngOnInit(): void {
+    // Pegamos o parâmetro `url` da rota
+    const url = this.activatedRoute.snapshot.paramMap.get('url');
+    if (url) {
+      // Buscar o projeto usando o `url` do sistema
+      this.sistemasService.buscarPorUrl(url).subscribe((sistema) => {
+        this.sistema = sistema;
+      });
+    }
+  }
 }
