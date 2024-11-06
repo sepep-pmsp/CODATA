ddde28f04cfbcd75647303fad3790ef109c7f4cf
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2024-11-06 15:54:53 -0300

    feat: add EstudosService to retrieve data from DataService
    Configured EstudosService to retrieve data from the centralized DataService.

--- src/app/components/project/estudo/estudos-service.service.ts
+++ src/app/components/project/estudo/estudos-service.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DataService } from '../../data-service.service';
+import { Estudos } from '../../data.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class EstudosServiceService {
+export class EstudosService {
+  
+  constructor(private dataService: DataService) {}
 
-  constructor() { }
+  getEstudos(): Observable<Estudos[]> {
+    return this.dataService.getEstudos();
+  }
+
+  buscarPorUrl(url: string): Observable<Estudos | undefined> {
+    return new Observable((observer) => {
+      this.dataService.getEstudos().subscribe((estudos) => {
+        const estudo = estudos.find(e => e.url === url);
+        observer.next(estudo);
+        observer.complete();
+      });
+    });
+  }
 }
