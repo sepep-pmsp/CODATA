2b04b4e15391c18fd103d5ca064d850c5034bddf
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2024-11-06 15:56:54 -0300

    feat: add DashboardService to retrieve data from DataService

--- src/app/components/project/dashboard/dashboard-service.service.ts
+++ src/app/components/project/dashboard/dashboard-service.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { DataService } from '../../data-service.service';
+import { Dashboard } from '../../data.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class DashboardServiceService {
+export class DashboardService {
+  
+  constructor(private dataService: DataService) {}
 
-  constructor() { }
+  getDashboards(): Observable<Dashboard[]> {
+    return this.dataService.getDashboard();
+  }
+
+  buscarPorUrl(url: string): Observable<Dashboard | undefined> {
+    return new Observable((observer) => {
+      this.dataService.getDashboard().subscribe((dashboard) => {
+        const dash = dashboard.find(e => e.url === url);
+        observer.next(dash);
+        observer.complete();
+      });
+    });
+  }
 }
