2282e7e7a3b9a660b050d8ea03f1efb8be956429
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2024-11-26 08:46:13 -0300

    feat: Template refactoring and dynamic content logic
    Added auxiliary methods to the component (isString, isArray, isMixedContent) for validating types in the template.
    Fixed parsing error in Angular when using === directly in the template.
    Template refactoring to dynamically render content in three formats:
    Plain text.
    List of items.
    Mixed content with text and list.
    Guarantee of greater readability and robustness when managing different types of content.

--- src/app/components/project/banco-dados/bd-page/bd-page.component.ts
+++ src/app/components/project/banco-dados/bd-page/bd-page.component.ts
@@ -3,55 +3,75 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-bd-page',
   templateUrl: './bd-page.component.html',
-  styleUrl: './bd-page.component.scss'
+  styleUrls: ['./bd-page.component.scss'],
 })
 export class BdPageComponent {
-[x: string]: any;
- base_dados =[
-  {class_name:'base_of_dados', title:'Base de Dados a', desription:'A base desses dados feitos...', link:'https://i.pinimg.com/474x/aa/2c/c6/aa2cc6a8122234a7e04b120f6151ea6e.jpg', name_btn:'abrir link', svg: 'assets/icons/link_icon.svg'},
-  {class_name:'base_of_dados', title:'Base de Dados b', desription:'A base desses dados feitos...', link:'https://i.pinimg.com/474x/aa/2c/c6/aa2cc6a8122234a7e04b120f6151ea6e.jpg', name_btn:'abrir link', svg: 'assets/icons/link_icon.svg'},
-  {class_name:'base_of_dados', title:'Base de Dados c', desription:'A base desses dados feitos...', link:'https://i.pinimg.com/474x/aa/2c/c6/aa2cc6a8122234a7e04b120f6151ea6e.jpg', name_btn:'abrir link', svg: 'assets/icons/link_icon.svg'},
-  {class_name:'base_of_dados', title:'Base de Dados d', desription:'A base desses dados feitos...', link:'https://i.pinimg.com/474x/aa/2c/c6/aa2cc6a8122234a7e04b120f6151ea6e.jpg', name_btn:'abrir link', svg: 'assets/icons/link_icon.svg'},
-  {class_name:'base_of_dados', title:'Base de Dados e', desription:'A base desses dados feitos...', link:'https://i.pinimg.com/474x/aa/2c/c6/aa2cc6a8122234a7e04b120f6151ea6e.jpg', name_btn:'abrir link', svg: 'assets/icons/link_icon.svg'},
-  {class_name:'base_of_dados', title:'Base de Dados f', desription:'A base desses dados feitos...', link:'https://i.pinimg.com/474x/aa/2c/c6/aa2cc6a8122234a7e04b120f6151ea6e.jpg', name_btn:'abrir link', svg: 'assets/icons/link_icon.svg'},
- ];
+  content_paragraph= [
+    {txt_1: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta minima doloribus quia nostrum explicabo, itaque aliquid culpa non, corrupti atque unde fugit hic. Quia delectus laudantium ipsa, assumenda hic possimus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius laboriosam, blanditiis fugit ad delectus, saepe et rem ullam aspernatur id sequi rerum optio aliquid eveniet. Cum voluptas dolorem dolore expedita! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas necessitatibus vero reprehenderit delectus tempore ratione, ut officiis culpa voluptates. Autem architecto neque libero magnam labore, alias iure id. Dolores, ab. '},
+    {txt_2:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta minima doloribus quia nostrum explicabo, itaque aliquid culpa non, corrupti atque unde fugit hic. Quia delectus laudantium ipsa, assumenda hic possimus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius laboriosam, blanditiis fugit ad delectus, saepe et rem ullam aspernatur id sequi rerum optio aliquid eveniet. Cum voluptas dolorem dolore expedita! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas necessitatibus vero reprehenderit delectus tempore ratione, ut officiis culpa voluptates. Autem architecto neque libero magnam labore, alias iure id. Dolores, ab. '},
+    {txt_3:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta minima doloribus quia nostrum explicabo, itaque aliquid culpa non, corrupti atque unde fugit hic. Quia delectus laudantium ipsa, assumenda hic possimus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius laboriosam, blanditiis fugit ad delectus, saepe et rem ullam aspernatur id sequi rerum optio aliquid eveniet. Cum voluptas dolorem dolore expedita! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas necessitatibus vero reprehenderit delectus tempore ratione, ut officiis culpa voluptates. Autem architecto neque libero magnam labore, alias iure id. Dolores, ab. '}
+  ];
 
- content_list=[
-  {txt:'campo', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit.',},
-  {txt:'autor', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit.',},
-  {txt:'versão', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit.',},
-  {txt:'última atualização', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit.',},
-  {txt:'data de criação', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit.',}
- ];
+  base_dados = [
+    { class_name: 'base_of_dados', title: 'Base de Dados a', description: 'A base desses dados feitos...', link: 'https://i.pinimg.com/474x/aa/2c/c6/aa2cc6a8122234a7e04b120f6151ea6e.jpg', name_btn: 'abrir link', svg: 'assets/icons/link_icon.svg' },
+    { class_name: 'base_of_dados', title: 'Base de Dados b', description: 'A base desses dados feitos...', link: 'https://i.pinimg.com/474x/aa/2c/c6/aa2cc6a8122234a7e04b120f6151ea6e.jpg', name_btn: 'abrir link', svg: 'assets/icons/link_icon.svg' },
+    { class_name: 'base_of_dados', title: 'Base de Dados c', description: 'A base desses dados feitos...', link: 'https://i.pinimg.com/474x/aa/2c/c6/aa2cc6a8122234a7e04b120f6151ea6e.jpg', name_btn: 'abrir link', svg: 'assets/icons/link_icon.svg' },
+  ];
 
- content_field_font = [
-  {text:'fonte', class_name_field:'content_font', img:'assets/logos/logo-prefeitura.svg'},
-  {text:'licença', class_name_field:'content_license', img:'assets/logos/Creative-Commons-Logo.png'}
- ]
+  content_list = [
+    { txt: 'campo', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+    { txt: 'autor', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  ];
+
+  content_field_font = [
+    { text: 'fonte', class_name_field: 'content_font', img: 'assets/logos/logo-prefeitura.svg' },
+    { text: 'licença', class_name_field: 'content_license', img: 'assets/logos/Creative-Commons-Logo.png' },
+  ];
 
   options = [
     'identificação',
     'distribuição',
     'qualidade',
     'ref.system',
-    'metadata'
+    'metadata',
   ];
 
   selectedOption: string = this.options[0];
   activeAnimation: { [key: string]: string } = {};
   boxAnimationClass: string = '';
 
-  contentMapping: { [key: string]: string | string[] } = {
-    identificação: 'ID: 4L3X-77Z Codinome: Operador Tactica-Delta, Imperio',
-    distribuição: 'Fabricante: Galactic Engineering Consortium (GEC) Modelo: DROID-7 Combatant Series Ano de Fabricação: 2 ABY (Após a Batalha de Yavin) Série: D9-TCD Localização Inicial: Sector Corellian (Plataforma C-56-31X)',
-    qualidade: 'Nível de Durabilidade: Alto (99.87% resistência a impactos e danos térmicos) Capacidade de Ação: 98% de eficiência em combate corpo a corpo e de longo alcance Sistema de Camuflagem: Camuflagem ativa, visibilidade reduzida em 75% durante manobras furtivas Fornecimento de Energia: Alimentado por cristal kyber modificado com 500 MW de potência por ciclo',
-    'ref.system': 'Core Processor: Vortex-12 Quantum Core Controle de Comunicação: HoloCom v9.5 Sistema de Navegação: D-SUB Advanced Tactical Nav Capacidade de Comunicação: Frequência 445-675 GHz Análise de Combate: IA de Aprendizado Autônomo (AFS-AI 5.0)',
-    metadata: 'Fabricante de Componentes: Galactic Defense Innovations (GDI) Protocolo de Combate: L.R.S.-Tactical V2.1 Status Atual: Operacional, última atualização de firmware: 6 horas atrás Armas Disponíveis: Blaster HZ-54 Heavy Sabre de luz de combate, blade-type Drones de suporte T-6'
+  contentMapping: {
+    [key: string]: { type: 'text' | 'list' | 'mixed'; data: string | string[] | { text: string; list: string[] } };
+  } = {
+    identificação: { type: 'text', data: 'Conteúdo detalhado sobre identificação.\nMais detalhes aqui.' },
+    distribuição: { type: 'list', data: ['Regiões\n- Norte\n- Sul', 'Cidades', 'População'] },
+    qualidade: { type: 'mixed', data: { text: 'Informações detalhadas sobre qualidade.\nDetalhes técnicos.', list: ['Critério 1', 'Critério 2', 'Critério 3'] } },
+    'ref.system': { type: 'text', data: 'Detalhes do sistema de referência.' },
+    metadata: { type: 'text', data: 'Metadata' },
   };
 
-  processedContent: string | string[] = this.contentMapping[this.selectedOption];
-  isArray(value: any): boolean {
-    return Array.isArray(value);
+  isString(content: any): content is string {
+    return typeof content === 'string';
+  }
+  
+  isArray(content: any): content is string[] {
+    return Array.isArray(content);
+  }
+  
+  isMixedContent(content: any): content is { text: string; list: string[] } {
+    return content && typeof content.text === 'string' && Array.isArray(content.list);
+  }
+
+  get processedContent(): string | string[] | { text: string; list: string[] } {
+    const content = this.contentMapping[this.selectedOption].data;
+    if (typeof content === 'string') {
+      return this.formatContent(content);
+    }
+    return content;
+  }
+
+  formatContent(content: string): string {
+    return content.replace(/\n/g, '<br>');
   }
 
   selectOption(option: string, direction: 'left' | 'right'): void {
@@ -59,7 +79,6 @@ export class BdPageComponent {
     this.activeAnimation = { [option]: direction === 'left' ? 'slide-left' : 'slide-right' };
     setTimeout(() => {
       this.selectedOption = option;
-      this.processedContent = this.contentMapping[option]; 
       this.boxAnimationClass = 'flip-vertical-left';
       this.activeAnimation = {};
     }, 500);
@@ -68,5 +87,4 @@ export class BdPageComponent {
       this.boxAnimationClass = '';
     }, 1000);
   }
-
 }
