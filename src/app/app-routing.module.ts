707b76ff9fab07f1b23150667104a4996fc0fe10
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2024-12-05 09:01:17 -0300

    implementing the assignment page route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { DashboardPageComponent } from './components/project/dashboard/dashboard
 import { EstudosPageComponent } from './components/project/estudo/estudos-page/estudos-page.component';
 import { BdPageComponent } from './components/project/banco-dados/bd-page/bd-page.component';
 import { TeamPageComponent } from './components/pages/team-page/team-page.component';
+import { AssignmentsPageComponent } from './components/pages/assignments-page/assignments-page.component';
 
 
 const routes: Routes = [
@@ -21,6 +22,7 @@ const routes: Routes = [
   { path: 'estudo/article/:url', component: EstudosPageComponent },
   { path: 'banco_de_dados/info/:url', component: BdPageComponent },
   { path: 'equipe', component: TeamPageComponent },
+  { path: 'atribuicoes', component: AssignmentsPageComponent },
 ];
 
 @NgModule({
