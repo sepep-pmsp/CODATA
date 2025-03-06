8ad75a982499194fa3a6c3ad99f61937c31a06c3
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2025-03-06 13:42:22 -0300

    fazendo a paginação

--- src/app/components/project/sistema/sistemas/sistemas.component.ts
+++ src/app/components/project/sistema/sistemas/sistemas.component.ts
@@ -10,23 +10,36 @@ import { Sistemas } from '../sistemas.model';
 })
 export class SistemasComponent implements OnInit {
   sistemas: Sistemas[] = [];
-  sistema: Sistemas | undefined;
-  
-    constructor(
-      private activatedRoute: ActivatedRoute,
-      private sistemasService: SistemasService
-    ) {}
-  
-    ngOnInit(): void {
-      this.sistemasService.getSistemas().subscribe((sistemas) => {
-        this.sistemas = sistemas;
-      });
-  
-      const id = this.activatedRoute.snapshot.paramMap.get('id');
-      if (id) {
-        this.sistemasService.buscarPorUrl(String(id)).subscribe((sistema) => {
-          this.sistema = sistema;
-        });
-      }
-    }
+  displayedProjects: Sistemas[] = [];
+  currentPage: number = 1;
+  itemsPerPage: number = 3;
+  totalPages: number = 1;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private sistemasService: SistemasService
+  ) {}
+
+  ngOnInit(): void {
+    this.sistemasService.getSistemas().subscribe((sistemas) => {
+      this.sistemas = sistemas;
+      this.calculateTotalPages();
+      this.updateDisplayedProjects();
+    });
+  }
+
+  calculateTotalPages(): void {
+    this.totalPages = Math.ceil(this.sistemas.length / this.itemsPerPage);
+  }
+
+  updateDisplayedProjects(): void {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.displayedProjects = this.sistemas.slice(startIndex, endIndex);
+  }
+
+  goToPage(page: number): void {
+    this.currentPage = page;
+    this.updateDisplayedProjects();
+  }
 }
