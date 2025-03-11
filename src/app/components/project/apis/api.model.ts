457d45b76810ea49a56157afd3ac7cc5da998e09
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2025-03-11 16:14:31 -0300

    create service page API

---
+++ src/app/components/project/apis/api.model.ts
@@ -0,0 +1,14 @@
+export interface Api{
+    id: string;
+    url: string;
+    titulo: string;
+    github?: string;
+    description: string;
+    texto: string;
+    texto2: string;
+    img?: string;
+    svg?: string;
+    listContents?: { title: string, content: string | string[] }[];
+    urlProducts?:{ url: string, title: string | string}[];
+    functions?:{metodo: string, endpoint: string, definicao: string, parametros: string, parametros_entrada: string, entrada: string, parametros_saida: string, saida: string | string}[];
+}
