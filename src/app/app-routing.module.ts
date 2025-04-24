5280e8cde2c4c1c24e3f557f2c6ba26c5c77265f
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2025-04-24 13:39:18 -0300

    Uso de Lazy Loading
    Para performance e escalabilidade, cada módulo funcional (projetos, dashboard, etc.) deve ser lazy-loaded.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,34 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/body/home/home.component';
-import { ContentPageComponent } from './components/body/content-page/content-page.component';
-import { ProjectsComponent } from './components/project/project';
-import { SistemasComponent } from './components/project/sistema/sistemas/sistemas.component';
-import { SistemasPageComponent } from './components/project/sistema/sistemas-page/sistemas-page.component';
-import { DashboardPageComponent } from './components/project/dashboard/dashboard-page/dashboard-page.component';
-import { EstudosPageComponent } from './components/project/estudo/estudos-page/estudos-page.component';
-import { BdPageComponent } from './components/project/banco-dados/bd-page/bd-page.component';
-import { TeamPageComponent } from './components/pages/team-page/team-page.component';
-import { AssignmentsPageComponent } from './components/pages/assignments-page/assignments-page.component';
-import { ContactComponent } from './components/pages/contact/contact.component';
-import { ApiComponent } from './components/project/apis/api/api.component';
-import { ApiPageComponent } from './components/project/apis/api-page/api-page.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { TeamComponent } from './pages/team/team.component';
+import { AtividadeComponent } from './pages/atividade/atividade.component';
+import { AtividadePopupComponent } from './pages/home/sections/atividade-popup/atividade-popup.component';
+
 
 
 const routes: Routes = [
   { path: '', pathMatch: 'full' , component: HomeComponent },
-  { path: 'pop-up/:slug', component: ContentPageComponent },
-  { path: 'projetos/:filter', component: ProjectsComponent },
-  { path: 'sistemas/:url', component: SistemasComponent },
-  { path: 'sistemas/detalhes/:url', component: SistemasPageComponent },
-  { path: 'dashboard/page/:url', component: DashboardPageComponent },
-  { path: 'estudo/article/:url', component: EstudosPageComponent },
-  { path: 'banco_de_dados/info/:url', component: BdPageComponent },
-  { path: 'equipe', component: TeamPageComponent },
-  { path: 'atribuicoes', component: AssignmentsPageComponent },
+  { path: 'pop-up/:slug', component: AtividadePopupComponent },
+  { path: 'equipe', component: TeamComponent },
+  { path: 'atribuicoes', component: AtividadeComponent },
   { path: 'contato', component: ContactComponent },
-  { path: 'API', component: ApiComponent },
-  { path: 'API/:url', component: ApiPageComponent },
+  {
+    path: 'projetos',
+    loadChildren: () =>
+      import('./pages/projects/projects.module').then(m => m.ProjectsModule)
+  },
 ];
 
 @NgModule({
